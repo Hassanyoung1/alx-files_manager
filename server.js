8a042a493b25d6abd9b28f270114764d0b307a9d
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-05-12 00:18:05 +0100

    Create

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 const routes = require('./routes/index');
 
 // Middleware to parse JSON requests
